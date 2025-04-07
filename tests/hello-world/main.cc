4c36da9a2b7d3cc8ab854ad58aa640b65d78da5e
Liu Peng <ppeng.lliu@gmail.com>
2025-04-07 22:27:12 +0800

    Reimplement as header only

--- tests/hello-world/main.cc
+++ tests/hello-world/main.cc
@@ -4,15 +4,15 @@
 #include "git.h"
 
 int main() {
-    if(git::IsPopulated()) {
-        if(git::AnyUncommittedChanges()) {
+    if(git::is_populated()) {
+        if(git::any_uncommitted_changes()) {
             std::cerr << "WARN: there were uncommitted changes at build-time." << std::endl;
         }
-        std::cout << "commit " << git::CommitSHA1() << " (" << git::Branch() << ")\n"
-                  << "describe " << git::Describe() << "\n"
-                  << "Author: " << git::AuthorName() << " <" << git::AuthorEmail() << ">\n"
-                  << "Date: " << git::CommitDate() << "\n\n"
-                  << git::CommitSubject() << "\n" << git::CommitBody() << std::endl;
+        std::cout << "commit " << git::commit_sha1() << " (" << git::branch() << ")\n"
+                  << "describe " << git::describe() << "\n"
+                  << "Author: " << git::author_name() << " <" << git::author_email() << ">\n"
+                  << "Date: " << git::commit_date() << "\n\n"
+                  << git::commit_subject() << "\n" << git::commit_body() << std::endl;
         return EXIT_SUCCESS;
     }
     else {
